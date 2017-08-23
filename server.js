6a2127ebd62470b7a85837f86e9c89126416ba9e
Ayu1511 <31167128+Ayu1511@users.noreply.github.com>
2017-08-23 19:58:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title: 'Article One',
+    heading: 'Article one',
+    date: '15 nov 1997',
+    content: `<p>
+                HELLO hie bye bye. HELLO hie bye bye. HELLO hie bye bye. HELLO hie bye bye. HELLO hie bye bye.
+            </p>
+            <p>
+                HELLO hie bye bye. HELLO hie bye bye. HELLO hie bye bye. HELLO hie bye bye. HELLO hie bye bye.
+            </p>
+            <p>
+                HELLO hie bye bye. HELLO hie bye bye. HELLO hie bye bye. HELLO hie bye bye. HELLO hie bye bye.
+            </p>`
+};
+
+function createTemplat (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viweport" content="width=device-width, initial-scale=1"/>
+             <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate();
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-    
+    res.send(createTemplate(articleOne));
 });
 
 
