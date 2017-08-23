eedfc7c97569d9f58578f0603bd48213626028b7
Ayu1511 <31167128+Ayu1511@users.noreply.github.com>
2017-08-23 21:50:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 var ele=document.getElementById('main');
 ele.innerHTML="New value";
 
+var img=document.getElementById('img');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
